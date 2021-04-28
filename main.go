f67008537916b0906a2c669d8a6924ba4aea58dd
Keithagy <68194558+Keithagy@users.noreply.github.com>
2021-04-28 18:19:31 +0800

    API structure planned out!

--- main.go
+++ main.go
@@ -11,20 +11,24 @@ import (
 )
 
 type Product struct {
-	name        string
-	description string
-	thumbnail   string
-	price       float64
-	quantity    int
+	Name        string
+	Description string
+	Thumbnail   string
+	Price       float64
+	Quantity    int
 }
 
 type Merchant struct {
-	Id       int
-	Products []Product
+	Id          int
+	Name        string
+	Description string
+	Products    []Product
 }
 
 const (
-	merchPath = "/storefront/v0/{merchantid}/{productid}"
+	homePath  = "/"
+	merchPath = "/merchants/{merchantid}"
+	prodPath  = "/products/{productid}"
 )
 
 var (
@@ -33,78 +37,99 @@ var (
 )
 
 func main() {
-	router.HandleFunc(merchPath, store)
+	router.HandleFunc(homePath, home)
+	router.HandleFunc(merchPath, merch)
+	router.HandleFunc(prodPath, prod)
 
 	fmt.Println("Listening at port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
 
 // Handler Functions
-func store(w http.ResponseWriter, r *http.Request) {
+func merch(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
 	// GET method - List products
-	// If no merchantid, list all merchants
-	// If no productid, list all products under a particular merchantid
+	// If no merchantid, show all merchants
+	// If have merchantid, show all products under merchantid (incl. nothing)
+	// If invalid merchantid, show error and redirect back to home page
 	// Callable by merchants and users
 	if r.Method == "GET" {
 		// Verify valid merchant ID
 		merchID, ok := params["merchantid"]
 		if !ok {
-			// No merchant ID supplied
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("404 - No merchant ID supplied"))
-			return
-		}
-
-		// Verify valid product ID
-		prodID, ok := params["productid"]
-		if !ok {
-			// Show all products under merchID; if invalid merchID handle error
-			merchProdsRows, err := db.Query("Select * FROM storefront_db.products WHERE merchid = ? AND quantity != 0", merchID)
-			defer merchProdsRows.Close()
+			// No merchant ID supplied. Show all merchants
+			merchantRows, err := db.Query("Select username, description FROM storefront_db.merchants")
 			if err != nil {
 				panic(err.Error())
 			}
+			defer merchantRows.Close()
 
-			var merchProds = make([]Product, 0)
-			for merchProdsRows.Next() {
-				var p Product
-				err = merchProdsRows.Scan(&p.name, &p.description, &p.thumbnail, &p.price, &p.quantity)
+			var merchants = make([]Merchant, 0)
+			for merchantRows.Next() {
+				var newMerchant Merchant
+				err = merchantRows.Scan(&newMerchant.Id, &newMerchant.Name, &newMerchant.Description, &newMerchant.Products)
 				if err != nil {
 					panic(err.Error())
 				}
-				merchProds = append(merchProds, p)
+				merchants = append(merchants, newMerchant)
 			}
 
-			if len(merchProds) == 0 {
-				w.WriteHeader(http.StatusNotFound)
-				w.Write([]byte("404 - No merchant for inputted merchant ID"))
+			// TODO: Execute some template passing in merchants slice
+			return
+		}
+
+		// Merchant ID supplied
+		// Show all products under merchID; if invalid merchID handle error
+		// TODO change query string
+		merchProdsRows, err := db.Query("Select * FROM storefront_db.products WHERE merchid = ? AND quantity != 0", merchID)
+		if err != nil {
+			panic(err.Error())
+		}
+		defer merchProdsRows.Close()
+
+		var merchProds = make([]Product, 0)
+		for merchProdsRows.Next() {
+			var p Product
+			err = merchProdsRows.Scan(&p.Name, &p.Description, &p.Thumbnail, &p.Price, &p.Quantity)
+			if err != nil {
+				// Valid merchant ID but no products under merchant ID
+				w.WriteHeader(http.StatusNoContent)
+				w.Write([]byte("204 - Valid merchant ID, but store is empty"))
 				return
 			}
+			merchProds = append(merchProds, p)
+		}
 
-		} else {
-			// Check if prodID is valid
-			// Show merchID -> prodID
+		if len(merchProds) == 0 {
+			// Invalid merchant ID inputted
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("404 - No merchant for inputted merchant ID"))
+			return
 		}
 	}
+	// POST method - Add a Merchant (ADMIN ONLY)
+	// PUT method - Edit / Remove existing Merchant (ADMIN ONLY)
+	// DELETE method - Delete merchant (ADMIN ONLY)
+}
 
-	// POST method - Add a Product to Store
-	// Valid input would REQUIRE a specific merchantid and productid
-	// Can only be called by merchants, on their own store (validation)
-
-	// PUT method - Edit / Remove existing Product
-	// Valid input would REQUIRE a specific merchantid and productid
-	// Can only be called by merchants, on their own store (validation)
-
-	// DELETE method - Delete the store (less important)
+func prod(w http.ResponseWriter, r *http.Request) {
+	// GET method: Show a particular product given productid, show all products otherwise (USER / MERCHANT)
+	// POST method: Add a Product (MERCHANT ONLY)
+	// PUT method: Update existing Product (MERCHANT ONLY)
+	// DELETE method: Delete existing Product (MERCHANT ONLY)
 }
 
 func cart(w http.ResponseWriter, r *http.Request) {
-}
-
-func merchants(w http.ResponseWriter, r *http.Request) {
+	// GET method: Show a user's entire cart (USER ONLY)
+	// POST method: Add a Product to a user's cart (USER ONLY)
+	// PUT method: Edit item in user's cart (USER ONLY)
+	// DELETE method: Delete existing Product (MERCHANT ONLY)
 }
 
 func users(w http.ResponseWriter, r *http.Request) {
+	// GET method: Username registry (ADMIN ONLY)
+	// POST method: Add new user (ADMIN ONLY)
+	// PUT method: Edit new user (ADMIN ONLY)
+	// DELETE method: Delete user (ADMIN ONLY)
 }
