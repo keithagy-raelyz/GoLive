59f2a339519ea65556f1cc48cbea9c8a2f442a66
Keithagy <68194558+Keithagy@users.noreply.github.com>
2021-04-29 18:06:20 +0800

    mid-refactor to discussed architecture

---
+++ app/product.go
@@ -0,0 +1,10 @@
+package app
+
+type Product struct {
+	Name        string
+	Id          int
+	Description string
+	Thumbnail   string
+	Price       float64
+	Quantity    int
+}
---
+++ app/user.go
@@ -0,0 +1,8 @@
+package app
+
+// Information attached to general users.
+type User struct {
+	Id    string
+	Name  string
+	Email string
+}
