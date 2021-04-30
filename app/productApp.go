ac618cfbe339178e0cde230cd55a8c6c5fac4afc
Keithagy <68194558+Keithagy@users.noreply.github.com>
2021-04-30 14:12:20 +0800

    testing Import cycle

--- app/productApp.go
+++ app/productApp.go
@@ -2,14 +2,14 @@ package app
 
 import "net/http"
 
-type Product struct {
-	Name        string
-	Id          int
-	Description string
-	Thumbnail   string
-	Price       float64
-	Quantity    int
-}
+// type Product struct {
+// 	Name        string
+// 	Id          int
+// 	Description string
+// 	Thumbnail   string
+// 	Price       float64
+// 	Quantity    int
+// }
 
 func (a *App) allProd(w http.ResponseWriter, r *http.Request) {
 
--- app/userApp.go
+++ app/userApp.go
@@ -1,8 +1,8 @@
 package app
 
-// Information attached to general users.
-type User struct {
-	Id    string
-	Name  string
-	Email string
-}
+// // Information attached to general users.
+// type User struct {
+// 	Id    string
+// 	Name  string
+// 	Email string
+// }
